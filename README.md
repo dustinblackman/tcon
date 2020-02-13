2439a5603b2a6fdf7f4218d24af4e70d7c4d75ef
Dustin Blackman <dev@dustinblackman.com>
2020-02-12 19:29:01 -0500

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+![tcon](assets/banner.jpg)
+
+A slightly lazy shell script to run parallel commands with tmux splits through a FIFO queue. This makes it super easy to run multiple jobs at the same time while monitoring progress. `tcon` uses uses [`tmux-xpanes`](https://github.com/greymd/tmux-xpanes#installation) under the hood. You can pass xpanes parameters to `tcon` after the concurrency limit to take full advantage of it.
+
+## Usage
+
+```sh
+  # Echo a line and sleep afterwards with a concurrency of 4. `-x` is passed along to tmux-xpanes.
+  seq 1 10 | (while read f; do echo "echo 'Sleeping for $f seconds' && sleep $f"; done;) | tcon 4 -x
+```
+
+## Installation
+
+```
+  curl -L https://raw.githubusercontent.com/dustinblackman/tcon/master/tcon.sh > /usr/local/bin/tcon
+  chmod +x /usr/local/bin/tcon
+```
+
+## [License](./LICENSE)
+
+MIT
---
+++ tcon.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+child() {
+  pipe="$1"
+
+  while true; do
+    if read line <$pipe; then
+      if [[ "$line" == 'tcon-quit' ]]; then
+        exit 0
+      fi
+
+      echo "$line"
+      (eval "$line")
+    fi
+  done
+}
+
+run() {
+  concurrency="$1"
+  shift;
+
+  echo "Starting up tmux panes with a concurrency of $concurrency"
+
+  FIFO=$(mktemp -t tcon-fifo)
+  rm $FIFO
+  mkfifo $FIFO
+
+  commands=$(cat -)
+  commands_count=$(echo "$commands" | wc -l | sed 's/ //g')
+
+  seq 1 "$concurrency" | xpanes "$@" -c "tcon child $FIFO"
+
+  count=0
+  echo "$commands" | while read f; do
+    count=$((count+1))
+    echo "$f" > "$FIFO"
+    echo "Running ${count}/${commands_count}: $f"
+  done
+
+  seq 1 "$concurrency" | while read f; do echo 'tcon-quit' > "$FIFO"; done;
+
+  # Lazy cleanup.
+  (sleep 2 && rm "$FIFO") &
+}
+
+if [[ "$1" == "child" ]]; then
+  child "$2"
+else
+  run "$@"
+fi
